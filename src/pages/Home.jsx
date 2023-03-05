076f5a49c93bdd8635bcf0d2ec62acaef7b98c9c
Thomas J. Vu <thomasjvu@gmail.com>
2023-03-05 01:45:25 -0500

    Add Threejs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, OrbitControls } from '@react-three/drei'
+import MeditationRoom from '../components/MeditationRoom'
+
+
 function Home() {
-    return (
-        <div>
-            <h1 className="text-4xl font-bold">Home</h1>
-            <h2 className="text-4xl">Something Else</h2>
-        </div>
-    )
+  return (
+    <div>
+            <Canvas>
+                <Suspense fallback={null}>
+                      <ambientLight intensity={0.1} />
+                    <MeditationRoom />
+                    <OrbitControls />
+                    {/* <Environment preset="sunset" background /> */}
+                </Suspense>
+            </Canvas>
+    </div>
+  )
 }
 
 export default Home
